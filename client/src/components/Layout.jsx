abda7e805cf0ecd0a85a6c5619b95d4ea18fec92
filippodelministro <filippodelministro@gmail.com>
2025-12-08 18:10:57 +0100

    cosmetical

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -10,16 +10,6 @@ import { Footer } from './Footer.jsx';
 import API from '../API.js';
 
 
-function NotFoundLayout(props) {
-    return (
-      <>
-        <h2>This route is not valid!</h2>
-        <Link to="/">
-          <Button variant="primary">Go back to the main page!</Button>
-        </Link>
-      </>
-    );
-}
 
 function LoginLayout(props) {
     return (
@@ -360,171 +350,6 @@ function StandingsLayout(props) {
   );
 }
 
-
-
-function MissionLayout(props) {
-  return (
-<>
-      <div className="flex-grow-1">
-        <Row>
-          <Col>
-            <Navigation loggedIn={props.loggedIn} user={props.user} logout={props.logout} />
-          </Col>
-        </Row>
-        
-        <div className="container my-2 flex-grow-5 d-flex justify-content-center">
-          <p className="text-muted">
-            Dai ma secondo te, cosa pensavi di trovarci qui? Un Manifesto Politico? 
-            La Magna Charta Libertatum? Una Dichiarazione di Indipendenza? <br/> La mission.. 
-            ti devi svegliare. <br /> <br />Siamo un torneo di calcetto, niente di speciale, 
-            il più bel torneo di calcetto a cui tu abbia mai partecipato o solo assistito. <br />
-            Niente di più. <br /> Una roba che non esiste in tutto il mondo. <br /> Niente da aggiungere. <br />
-            Un torneo unico, un’esperienza indimenticabile, qualcosa che non hai mai visto prima. <br />
-            Tutto qui.
-          </p>
-      </div>
-      </div>
-      <Row>
-        <Col>
-          <Footer className="mt-auto" />
-        </Col>
-      </Row>
-    </>
-  );
-}
-
-
-function ContactsLayout(props) {
-  return (
-<>
-      <div className="flex-grow-1">
-        <Row>
-          <Col>
-            <Navigation loggedIn={props.loggedIn} user={props.user} logout={props.logout} />
-          </Col>
-        </Row>
-        
-        <div className="container my-2 flex-grow-5 d-flex justify-content-center">
-          <h1>CONTATTI</h1>
-      </div>
-      </div>
-      <Row>
-        <Col>
-          <Footer className="mt-auto" />
-        </Col>
-      </Row>
-    </>
-  );
-}
-
-function HistoryLayout(props) {
-  return (
-<>
-      <div className="flex-grow-1">
-        <Row>
-          <Col>
-            <Navigation loggedIn={props.loggedIn} user={props.user} logout={props.logout} />
-          </Col>
-        </Row>
-        
-        <div className="container my-2 flex-grow-5 d-flex justify-content-center">
-          <h1>HISTORY</h1>
-      </div>
-      </div>
-      <Row>
-        <Col>
-          <Footer className="mt-auto" />
-        </Col>
-      </Row>
-    </>
-  );
-}
-
-function ArchiveLayout(props) {
-  return (
-<>
-      <div className="flex-grow-1">
-        <Row>
-          <Col>
-            <Navigation loggedIn={props.loggedIn} user={props.user} logout={props.logout} />
-          </Col>
-        </Row>
-        
-        <div className="container my-2 flex-grow-5 d-flex justify-content-center">
-          <h1>ARCHIVIO???</h1>
-      </div>
-      </div>
-      <Row>
-        <Col>
-          <Footer className="mt-auto" />
-        </Col>
-      </Row>
-    </>
-  );
-}
-
-
-function PolicyLayout(props) {
-  return (
-    <div className="d-flex flex-column min-vh-100">
-
-      <Navigation 
-        loggedIn={props.loggedIn} 
-        user={props.user} 
-        logout={props.logout}
-      />
-
-      <div className="container my-5 flex-grow-1">
-
-        <Row className="g-4">
-          <div>
-            <h4>Chi siamo</h4>
-            <p>L’indirizzo del nostro sito web è: https://glapthetorneo.it</p>
-
-            <h4>Commenti</h4>
-            <p>Quando i visitatori lasciano commenti sul sito, raccogliamo i dati mostrati nel modulo dei commenti oltre all’indirizzo IP del visitatore e la stringa dello user agent del browser per facilitare il rilevamento dello spam.</p>
-            <p>Una stringa anonimizzata creata a partire dal tuo indirizzo email (altrimenti detta hash) può essere fornita al servizio Gravatar per vedere se lo stai usando. La privacy policy del servizio Gravatar è disponibile qui: https://automattic.com/privacy/. Dopo l’approvazione del tuo commento, la tua immagine del profilo è visibile al pubblico nel contesto del tuo commento.</p>
-
-            <h4>Media</h4>
-            <p>Se carichi immagini sul sito web, dovresti evitare di caricare immagini che includono i dati di posizione incorporati (EXIF GPS). I visitatori del sito web possono scaricare ed estrarre qualsiasi dato sulla posizione dalle immagini sul sito web.</p>
-
-            <h4>Cookie</h4>
-            <p>Se lasci un commento sul nostro sito, puoi scegliere di salvare il tuo nome, indirizzo email e sito web nei cookie. Sono usati per la tua comodità in modo che tu non debba inserire nuovamente i tuoi dati quando lasci un altro commento. Questi cookie dureranno per un anno.</p>
-            <p>Se visiti la pagina di login, verrà impostato un cookie temporaneo per determinare se il tuo browser accetta i cookie. Questo cookie non contiene dati personali e viene eliminato quando chiudi il browser.</p>
-            <p>Quando effettui l’accesso, verranno impostati diversi cookie per salvare le tue informazioni di accesso e le tue opzioni di visualizzazione dello schermo. I cookie di accesso durano due giorni mentre i cookie per le opzioni dello schermo durano un anno. Se selezioni “Ricordami”, il tuo accesso persisterà per due settimane. Se esci dal tuo account, i cookie di accesso verranno rimossi.</p>
-            <p>Se modifichi o pubblichi un articolo, un cookie aggiuntivo verrà salvato nel tuo browser. Questo cookie non include dati personali, ma indica semplicemente l’ID dell’articolo appena modificato. Scade dopo 1 giorno.</p>
-            <p>Inoltre, noi i cookies li mangiamo col milk.</p>
-
-            <h4>Contenuto incorporato da altri siti web</h4>
-            <p>Gli articoli su questo sito possono includere contenuti incorporati (ad esempio video, immagini, articoli, ecc.). I contenuti incorporati da altri siti web si comportano esattamente allo stesso modo come se il visitatore avesse visitato l’altro sito web.</p>
-            <p>Questi siti web possono raccogliere dati su di te, usare cookie, integrare ulteriori tracciamenti di terze parti e monitorare l’interazione con essi, incluso il tracciamento della tua interazione con il contenuto incorporato se hai un account e sei connesso a quei siti web.</p>
-
-            <h4>Con chi condividiamo i tuoi dati</h4>
-            <p>Se richiedi una reimpostazione della password, il tuo indirizzo IP verrà incluso nell’email di reimpostazione.</p>
-
-            <h4>Per quanto tempo conserviamo i tuoi dati</h4>
-            <p>Se lasci un commento, il commento e i relativi metadati vengono conservati a tempo indeterminato. È così che possiamo riconoscere e approvare automaticamente eventuali commenti successivi invece di tenerli in una coda di moderazione.</p>
-            <p>Per gli utenti che si registrano sul nostro sito web (se presenti), memorizziamo anche le informazioni personali che forniscono nel loro profilo utente. Tutti gli utenti possono vedere, modificare o eliminare le loro informazioni personali in qualsiasi momento (eccetto il loro nome utente che non possono cambiare). Gli amministratori del sito web possono anche vedere e modificare queste informazioni.</p>
-
-            <h4>Quali diritti hai sui tuoi dati</h4>
-            <p>Se hai un account su questo sito, o hai lasciato commenti, puoi richiedere di ricevere un file esportato dal sito con i dati personali che abbiamo su di te, compresi i dati che ci hai fornito. Puoi anche richiedere che cancelliamo tutti i dati personali che ti riguardano. Questo non include i dati che siamo obbligati a conservare per scopi amministrativi, legali o di sicurezza.</p>
-
-            <h4>Dove i tuoi dati sono inviati</h4>
-            <p>I commenti dei visitatori possono essere controllati attraverso un servizio di rilevamento automatico dello spam.</p>
-          </div>
-        </Row>
-      </div>
-
-      <Footer />
-    </div>
-
-
-
-    
-  );
-
-}
-
 function TeamsLayout(props) {
   const [teams, setTeams] = useState([]);
   const [error, setError] = useState(null);
@@ -581,6 +406,217 @@ function TeamsLayout(props) {
   );
 }
 
+function MissionLayout(props) {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navigation
+        loggedIn={props.loggedIn}
+        user={props.user}
+        logout={props.logout}
+      />
+
+      <div className="container my-5 flex-grow-1">
+
+        {/* Title */}
+        <div className="d-flex align-items-center justify-content-between mb-4">
+          <h1>La mission</h1>
+        </div>
+
+        {/* Content */}
+        <div>
+          <Row className="g-4">
+            <div>
+              <p className="text-muted">
+                Dai ma secondo te, cosa pensavi di trovarci qui? Un Manifesto Politico? 
+                La Magna Charta Libertatum? Una Dichiarazione di Indipendenza? <br/> La mission.. 
+                ti devi svegliare. <br /> <br />Siamo un torneo di calcetto, niente di speciale, 
+                il più bel torneo di calcetto a cui tu abbia mai partecipato o solo assistito. <br />
+                Niente di più. <br /> Una roba che non esiste in tutto il mondo. <br /> Niente da aggiungere. <br />
+                Un torneo unico, un’esperienza indimenticabile, qualcosa che non hai mai visto prima. <br />
+                Tutto qui.
+              </p>
+            </div>
+          </Row>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+function ContactsLayout(props) {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navigation
+        loggedIn={props.loggedIn}
+        user={props.user}
+        logout={props.logout}
+      />
+
+      <div className="container my-5 flex-grow-1">
+
+        <div className="d-flex align-items-center justify-content-between mb-4">
+          <h1>Contacts</h1>
+        </div>
+
+        <div>
+        </div>
+
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+function HistoryLayout(props) {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navigation
+        loggedIn={props.loggedIn}
+        user={props.user}
+        logout={props.logout}
+      />
+
+      <div className="container my-5 flex-grow-1">
+
+        <div className="d-flex align-items-center justify-content-between mb-4">
+          <h1>Hisotry</h1>
+        </div>
+
+        <div>
+        </div>
+
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+function ArchiveLayout(props) {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navigation
+        loggedIn={props.loggedIn}
+        user={props.user}
+        logout={props.logout}
+      />
+
+      <div className="container my-5 flex-grow-1">
+
+        <div className="d-flex align-items-center justify-content-between mb-4">
+          <h1>Archive</h1>
+        </div>
+
+        <div>
+        </div>
+
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+function PolicyLayout(props) {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navigation
+        loggedIn={props.loggedIn}
+        user={props.user}
+        logout={props.logout}
+      />
+
+      <div className="container my-5 flex-grow-1">
+
+        {/* Title */}
+        <div className="d-flex align-items-center justify-content-between mb-4">
+          <h1>Privacy Policy</h1>
+        </div>
+
+        {/* Content */}
+        <div>
+          <Row className="g-4">
+            <div>
+              <h4>Chi siamo</h4>
+              <p>L’indirizzo del nostro sito web è: https://glapthetorneo.it</p>
+
+              <h4>Commenti</h4>
+              <p>
+                Quando i visitatori lasciano commenti sul sito, raccogliamo i dati
+                mostrati nel modulo dei commenti oltre all’indirizzo IP del
+                visitatore e la stringa dello user agent del browser per facilitare
+                il rilevamento dello spam.
+              </p>
+              <p>
+                Una stringa anonimizzata creata a partire dal tuo indirizzo email può
+                essere fornita al servizio Gravatar per vedere se lo stai usando.
+                La privacy policy è disponibile su automattic.com/privacy.
+              </p>
+
+              <h4>Media</h4>
+              <p>
+                Se carichi immagini sul sito, evita quelle con dati EXIF GPS.
+              </p>
+
+              <h4>Cookie</h4>
+              <p>
+                Se lasci un commento puoi salvare nome/email/sito nei cookie per 1 anno.
+              </p>
+              <p>
+                Visita la pagina login → cookie temporaneo che verifica se il browser li accetta.
+              </p>
+              <p>
+                Quando accedi → cookie per accesso e preferenze schermo.
+              </p>
+              <p>Inoltre, noi i cookies li mangiamo col milk.</p>
+
+              <h4>Contenuto incorporato</h4>
+              <p>
+                Contenuti incorporati da altri siti si comportano come visite dirette a quei siti.
+              </p>
+
+              <h4>Con chi condividiamo i tuoi dati</h4>
+              <p>
+                Richiesta reset password → il tuo IP viene incluso nell’email.
+              </p>
+
+              <h4>Per quanto tempo conserviamo i tuoi dati</h4>
+              <p>
+                I commenti e metadati sono conservati indefinitamente per riconoscere futuri commenti.
+              </p>
+
+              <h4>Quali diritti hai sui tuoi dati</h4>
+              <p>
+                Puoi richiedere esportazione o cancellazione dei tuoi dati personali.
+              </p>
+
+              <h4>Dove i tuoi dati sono inviati</h4>
+              <p>
+                I commenti possono essere controllati attraverso un servizio antispam.
+              </p>
+            </div>
+          </Row>
+        </div>
+
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+function NotFoundLayout(props) {
+    return (
+      <>
+        <h2>This route is not valid!</h2>
+        <Link to="/">
+          <Button variant="primary">Go back to the main page!</Button>
+        </Link>
+      </>
+    );
+}
 
 
 export { HomeLayout, StandingsLayout, NotFoundLayout, LoginLayout, HistoryLayout, ArchiveLayout, MissionLayout, ContactsLayout, PolicyLayout, TeamsLayout };
